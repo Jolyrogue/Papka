f1a5c440efc8b946700126e1910db3875691f552
Jolyrogue <37841374+Jolyrogue@users.noreply.github.com>
2018-09-08 23:31:42 +0300

    first commit

---
+++ src/main/java/lab1/cat.java
@@ -0,0 +1,53 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * Test class for first lab of programming course
+ * in Sevastopol State University
+ *
+ *
+ * @author Batura Evgenii
+ * @version 1.0
+ */
+
+public class cat {
+    public static void main(String[] args){
+        String firstName = "Евгений";
+        String middleName = "Валерьевич";
+        String lastName = "Батура";
+        String teacherfirstName = "Татьяна";
+        String teachermiddleName = "Викторовна";
+        String teacherlastName = "Волкова";
+        String groupCode = "ИВТ/б-12о";
+        Date date = new Date();
+        int variant = 4;
+        int numberSeatsInTheCinema = 1500;
+        int numberRooms = 5;
+        int numberSeatsInTheRoom = numberSeatsInTheCinema / numberRooms;
+
+        String fullName = fullName(lastName, firstName, middleName);
+        String fullTeacherName = fullName(teacherfirstName, teachermiddleName, teacherlastName);
+
+        System.out.println("Программу выполнил:\n" + fullName);
+        System.out.println("Шифр группы: \n" + groupCode);
+        System.out.println("Дата: \n" + date);
+        System.out.println("Вариант номер: \n" + variant);
+        System.out.println("Количество мест в Кинотеатре \n" + numberSeatsInTheCinema);
+        System.out.println("Количество залов \n" + numberRooms);
+        System.out.println("Число мест в зале \n" + numberSeatsInTheRoom);
+        System.out.println("Проверил: \n" + fullTeacherName );
+
+
+
+    }
+
+
+
+
+    public static String fullName(String a, String b, String c)
+    {
+        return a + " " + b + " " + c;
+    }
+}
+
